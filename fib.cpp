272d8c4fa7ddf703ab92702c6e9b30dad10a9423
Andrey <vavilovo64@gmail.com>
2022-03-08 00:09:08 +0400

    Добавил рекурсивную функцию fib

--- fib.cpp
+++ fib.cpp
@@ -2,7 +2,12 @@
 
 using namespace std;
 
+int fib(int x) {
+    if (x == 1 || x == 2) return 1;
+    return fib(x - 1) + fib(x - 2);
+}
+
 int main() {
-    cout << "Hello, World!";
+    cout << fib(10);
     return 0;
 }
